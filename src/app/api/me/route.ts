b9a7811190d1f5e019a44ff692d195cfcc99b000
Keno217 <KVelagic22@gmail.com>
2025-09-30 20:42:50 -0400

    fix(auth): added try/catch to endpoint api/me

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -2,29 +2,38 @@ import { NextRequest, NextResponse } from 'next/server';
 import pool from '@/lib/db';
 
 export async function GET(req: NextRequest) {
-  const sessionId = req.cookies.get('session')?.value;
+  try {
+    const sessionId = req.cookies.get('session')?.value;
 
-  if (!sessionId) return NextResponse.json({ user: null }, { status: 200 });
+    if (!sessionId) return NextResponse.json({ user: null }, { status: 200 });
 
-  const { rows } = await pool.query(
-    `
-    SELECT users.id,
-    users.username
-    FROM users
-    INNER JOIN sessions
-      ON sessions.user_id = users.id
-    WHERE sessions.id = $1
-      AND expires_at > NOW();
-    `,
-    [sessionId]
-  );
+    const { rows } = await pool.query(
+      `
+      SELECT users.id,
+      users.username
+      FROM users
+      INNER JOIN sessions
+        ON sessions.user_id = users.id
+      WHERE sessions.id = $1
+        AND expires_at > NOW();
+      `,
+      [sessionId]
+    );
 
-  if (rows.length === 0) {
-    return NextResponse.json({ user: null }, { status: 200 });
+    if (rows.length === 0) {
+      return NextResponse.json({ user: null }, { status: 200 });
+    }
+
+    return NextResponse.json(
+      { user: { id: rows[0].id, username: rows[0].username } },
+      { status: 200 }
+    );
+    
+  } catch (err) {
+    console.error('DB session query error:', err);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
   }
-
-  return NextResponse.json(
-    { user: { id: rows[0].id, username: rows[0].username } },
-    { status: 200 }
-  );
 }
