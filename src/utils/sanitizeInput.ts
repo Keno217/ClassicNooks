1b9222a9014f8d0ecc8e1fa92d1179684391af2c
Keno217 <KVelagic22@gmail.com>
2025-10-10 01:18:36 -0400

    refactor(util): reduced max length for user input

--- src/utils/sanitizeInput.ts
+++ src/utils/sanitizeInput.ts
@@ -1,6 +1,6 @@
 /* Sanitize % and _ in book title input */
 export function sanitizeInput(input: string): string {
-  const MAX_LENGTH: number = 200;
+  const MAX_LENGTH: number = 100;
 
   return input
     .slice(0, MAX_LENGTH)
