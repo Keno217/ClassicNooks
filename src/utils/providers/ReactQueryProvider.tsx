20ee6573f157b32d3866d8dafaf0c8c20f902ae5
Keno217 <KVelagic22@gmail.com>
2025-10-11 21:31:58 -0400

    fix(frontend): removed refetch when reentering page/alt tabbing

--- src/utils/providers/ReactQueryProvider.tsx
+++ src/utils/providers/ReactQueryProvider.tsx
@@ -4,7 +4,16 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
 
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions : {
+      queries : {
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+        refetchOnMount: false,
+        staleTime: Infinity,
+      }
+    }
+  }));
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
